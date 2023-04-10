44857a7c20075ded507739ce7b8840c3d0b935a4
bharat <ladebharat9@gmail.com>
2023-04-10 23:09:45 +0530

    first commit

---
+++ src/main/java/com/jspiders/servlet/servlets/Form.java
@@ -0,0 +1,33 @@
+package com.jspiders.servlet.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/names")
+public class Form extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	public Form() {
+		super();
+	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		String name=request.getParameter("Stdname");
+		writer.println("<h1> Welcome "+ name + "...!!!</h1>");
+
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		doGet(request, response);
+	}
+
+}
